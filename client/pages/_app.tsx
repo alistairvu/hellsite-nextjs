28b8d0ed1ff861508f412e90283468f308a16046
Alistair Vu <alistairvu@gmail.com>
2021-05-27 12:16:15 +0700

    Finish tailwind setup

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,16 @@
 import { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 
+const queryClient = new QueryClient();
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  </RecoilRoot>
 );
 
 export default MyApp;
---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
